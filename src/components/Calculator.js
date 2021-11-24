dcecc1bfca3debd7d069a19d738ea562a59d6645
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-11-24 13:09:25 +0200

    Add: operations functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,50 @@
 import React from 'react';
-
+// import operate from '../logic/operate';
+import calculate from '../logic/calculate';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      next: null,
+      total: null,
+    };
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(event) {
+    this.setState((state) => calculate(state, event.target.innerText));
+  }
+
   render() {
+    const { next, total } = this.state;
     return (
       <div className="calculator">
         <div className="calc-output">
-          <div className="calc-result"> 0 </div>
+          <div className="calc-result">
+            {next || (total || '0')}
+          </div>
         </div>
         <div className="calc-grid">
-          <button type="button"> AC </button>
-          <button type="button"> +/- </button>
-          <button type="button"> % </button>
-          <button type="button" className="logic-operators"> ÷ </button>
-          <button type="button"> 7 </button>
-          <button type="button"> 8 </button>
-          <button type="button"> 9 </button>
-          <button type="button" className="logic-operators"> x </button>
-          <button type="button"> 4 </button>
-          <button type="button"> 5 </button>
-          <button type="button"> 6 </button>
-          <button type="button" className="logic-operators"> - </button>
-          <button type="button"> 1 </button>
-          <button type="button"> 2 </button>
-          <button type="button"> 3 </button>
-          <button type="button" className="logic-operators"> + </button>
-          <button type="button" id="zero"> 0 </button>
-          <button type="button"> . </button>
-          <button type="button" className="logic-operators"> = </button>
+          <button type="button" onClick={this.handler}> AC </button>
+          <button type="button" onClick={this.handler}> +/- </button>
+          <button type="button" onClick={this.handler}> % </button>
+          <button type="button" className="logic-operators" onClick={this.handler}> ÷ </button>
+          <button type="button" onClick={this.handler}>7</button>
+          <button type="button" onClick={this.handler}> 8 </button>
+          <button type="button" onClick={this.handler}> 9 </button>
+          <button type="button" className="logic-operators" onClick={this.handler}> x </button>
+          <button type="button" onClick={this.handler}> 4 </button>
+          <button type="button" onClick={this.handler}> 5 </button>
+          <button type="button" onClick={this.handler}> 6 </button>
+          <button type="button" className="logic-operators" onClick={this.handler}> - </button>
+          <button type="button" onClick={this.handler}> 1 </button>
+          <button type="button" onClick={this.handler}> 2 </button>
+          <button type="button" onClick={this.handler}> 3 </button>
+          <button type="button" className="logic-operators" onClick={this.handler}> + </button>
+          <button type="button" id="zero" onClick={this.handler}> 0 </button>
+          <button type="button" onClick={this.handler}> . </button>
+          <button type="button" className="logic-operators" onClick={this.handler}> = </button>
         </div>
       </div>
 
